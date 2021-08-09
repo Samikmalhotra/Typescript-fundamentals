6ddd331f551b78cf4d57e42104c699b952e30f4b
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-10 05:02:29 +0530

    completed variables

--- 2-features/annotations/1-variables.ts
+++ 2-features/annotations/1-variables.ts
@@ -40,3 +40,23 @@ const newCoordinates: {x:number; y:number} = JSON.parse(json)
 console.log(newCoordinates);
 // newCoordinates.dbnuai
 // Error
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ['red','green','blue']
+let foundWord:boolean;
+
+for(let i=0;i<words.length;i++){
+  if(words[i] === 'green'){
+    foundWord = true;
+  }
+}
+
+// Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for(let i=0; i<numbers.length;i++){
+  if(numbers[i] > 0){
+    numberAboveZero = numbers[i]; 
+  }
+}
