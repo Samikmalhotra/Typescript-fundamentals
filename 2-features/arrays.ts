8be684e421fa92898950905bdbdb352332313a79
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-10 05:48:05 +0530

    initialised arrays

---
+++ 2-features/arrays.ts
@@ -0,0 +1,11 @@
+const carMakers = ['ford', 'toyota', 'honda']
+
+const carMaker:string[] = []
+
+const dates = [new Date(), new Date()]
+
+const carsByMake = [
+  ['f150'],
+  ['corollaa'],
+  ['camaro']
+]
