0bc0cb92b33fc5ade8c546ac7d462a08306f7725
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-10 06:31:02 +0530

    initialised interface

--- 2-features/4-interfaces.ts
+++ 2-features/4-interfaces.ts
@@ -3,3 +3,11 @@ const oldCivic = {
   year: 2000,
   broken: true
 };
+
+const printVehicle = (vehicle: {name: string; year: number; broken: boolean}):void => {
+  console.log(vehicle.name);
+  console.log(vehicle.year);
+  console.log(vehicle.broken);
+}
+
+printVehicle(oldCivic);
