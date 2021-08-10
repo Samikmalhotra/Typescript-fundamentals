0d10140d3269152b61be3af9688810b480b814f7
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-10 06:42:37 +0530

    completed interfaces

--- 2-features/4-interfaces.ts
+++ 2-features/4-interfaces.ts
@@ -1,13 +1,25 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+  summary(): string;
+}
+
 const oldCivic = {
   name: 'Civic',
   year: 2000,
-  broken: true
+  broken: true,
+  summary(): string {
+    return this.name + ' is broken';
+  }
 };
 
-const printVehicle = (vehicle: {name: string; year: number; broken: boolean}):void => {
+const printVehicle = (vehicle: Vehicle):void => {
   console.log(vehicle.name);
   console.log(vehicle.year);
   console.log(vehicle.broken);
 }
 
 printVehicle(oldCivic);
+
+// Make interfaces reusable
