2320283f256e2d8eb26effd77371f795580fef84
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-10 05:43:21 +0530

    added object annotations

---
+++ 2-features/annotations/3-objects.ts
@@ -0,0 +1,14 @@
+const profile ={
+  names:'Alex',
+  age:20,
+  coords:{
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+     this.age = age;
+  }
+};
+
+const {age, names}:{age: number, names:string} = profile;
+const {coords: {lat,lng}}: {coords:{lat:number; lng:number}} = profile;
