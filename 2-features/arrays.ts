22538afa9c7e442b8a7963191c7a714ba2b528f6
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-10 06:00:26 +0530

    completed arrays

--- 2-features/arrays.ts
+++ 2-features/arrays.ts
@@ -9,3 +9,24 @@ const carsByMake = [
   ['corollaa'],
   ['camaro']
 ]
+
+//  Help with inference when extracting values
+
+const newCar = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values, following is an error
+carMakers.push(100);
+
+// Help with map
+carMakers.map((car:string):string => {
+  return car;
+})
+
+// Flexible types
+const importantDates = [new Date(), '2030-10-10'];
+const newDates: (Date | string)[] = [new Date()];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
+// following is an error
+importantDates.push(100);
