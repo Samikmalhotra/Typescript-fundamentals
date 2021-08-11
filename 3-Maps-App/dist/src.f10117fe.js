eeb26cdf067f9a01f066c2f5b7f5f8ad99aba569
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-11 08:51:22 +0530

    updated infoWindow

--- 3-Maps-App/dist/src.f10117fe.js
+++ 3-Maps-App/dist/src.f10117fe.js
@@ -136893,6 +136893,10 @@ var User = function () {
     };
   }
 
+  User.prototype.markerContent = function () {
+    return "User Name: " + this.name;
+  };
+
   return User;
 }();
 
@@ -136922,6 +136926,10 @@ var Company = function () {
       lng: parseFloat(faker_1.default.address.longitude())
     };
   }
+
+  Company.prototype.markerContent = function () {
+    return "<div>\n    <h1>Company Name: " + this.companyName + "</h1>\n    <h3>Catchphrase: " + this.catchPhrase + "</h3>\n    </div>";
+  };
 
   return Company;
 }();
@@ -136960,7 +136968,7 @@ var CustomMap = function () {
 
   ;
 
-  CustomMap.prototype.addEventListener = function () {};
+  CustomMap.prototype.addListener = function () {};
 
   return CustomMap;
 }();
@@ -136969,7 +136977,7 @@ exports.CustomMap = CustomMap;
 
 (function () {
   var infoWindow = new google.maps.InfoWindow({
-    content: 'Hi there'
+    content: mappable.markerContent()
   });
   infoWindow.open(_this.googleMap, marker);
 });
@@ -137019,7 +137027,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44617" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35205" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- 3-Maps-App/src/CustomMap.ts
+++ 3-Maps-App/src/CustomMap.ts
@@ -8,7 +8,8 @@ interface Mappable{
   location: {
     lat: number;
     lng: number;
-  }
+  };
+  markerContent(): string
 }
 export class CustomMap{
   private googleMap: google.maps.Map;
@@ -23,8 +24,7 @@ export class CustomMap{
     })
   }
   
-
-  addMarker(mappable: Mappable | Company):void{
+  addMarker(mappable: Mappable):void{
     const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
@@ -34,11 +34,11 @@ export class CustomMap{
     })
   };
 
-  marker.addEventListener('click', () => {
+  marker.addListener('click', () => {
   const infoWindow = new google.maps.InfoWindow({
-      content: 'Hi there'
+      content: mappable.markerContent()
   });
   infoWindow.open(this.googleMap, marker);
-  }
+  };
 
 }
